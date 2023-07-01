8752e78bf5656b77492e8358a6ba16b31b260933
Iffa Khalisha <iffakhalisa99@gmail.com>
2023-07-02 00:37:08 +0700

    latihan cypress

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/support/e2e",
+    supportFile: false,
+  },
+});
---
+++ cypress/support/e2e/firstTest.spec.js
@@ -0,0 +1,22 @@
+describe("My First Test", () => {
+  it("Get commands querying", () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("Kitchen Sink");
+
+    cy.contains("get").click();
+
+    cy.get("#query-btn").click();
+  });
+  it("input Test to form field", () => {
+    cy.visit("https://example.cypress.io");
+
+    cy.contains("type").click();
+
+    cy.url().should("include", "/commands/actions");
+
+    cy.get("#email1")
+      .type("fakeemail@gmail.com")
+      .should("have.value", "fakeemail@gmail.com");
+  });
+});
